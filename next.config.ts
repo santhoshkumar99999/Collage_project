cc9d1c16150c3a4c388b9efb645df54feb683349
Hxhd hxhx <ssanthosh117kuar@gmail.com>
2025-09-26 03:30:13 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 require('dotenv').config({ path: './.env' });
@@ -25,7 +26,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https,
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
